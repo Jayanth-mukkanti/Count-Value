dfb3fb47b4fe812d622e3c8491afc5a503d97d43
Jayanth-mukkanti <144653457+Jayanth-mukkanti@users.noreply.github.com>
2023-11-04 18:27:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react"
+import "./App.css"
+
+const App = () => {
+  const [state, changeState] = useState(0)
+
+  return(
+
+    <div class="divElement">
+    <p class="para"> Value </p>
+    <p class="para-1">{state}</p>
+    <button onClick={() => {changeState(state + 1)}}> + </button> 
+    <button onClick={() => {changeState(state - 1)}}> - </button>
+    </div>
+
+  )
+}
+
+export default App;
